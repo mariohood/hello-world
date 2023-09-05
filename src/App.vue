cb5252794ed898dd22e6535c2f20451052d1686f
mariohood <manhattan199@hotmail.com>
2023-09-04 21:12:04 -0300

    Key Attribute - aulas

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  
+  <h2 v-for="(name, index) in names" :key="name">{{ index }} {{ name }}</h2>
+  <h2 v-for="name in fullNames" :key="name.first">{{ name.first }} {{ name.last }}</h2>
+  <div v-for="actor in actors" :key="actor.name">
+    <h2>{{ actor.name }}</h2>
+    <h3 v-for="movie in actor.movies" :key="movie"> {{ movie }}</h3>
+  </div>
+  <h2 v-for="(value, key, index) in myInfo" :key="value"> {{ index }} {{ key }} {{ value }}</h2>
+  <template v-for="name in names" :key="name">
+    <h2>{{ name }}</h2>
+    <hr />
+  </template>
 </template>
 
 <script>
@@ -7,7 +17,30 @@ export default {
   name: 'App',
   data () {
     return {
-     
+      names: ['Bruce', 'Clark', 'Diana'],
+      fullNames: [
+        { first: 'Bruce', last: 'Wayne' },
+        { first: 'Clark', last: 'Kent' },
+        { first: 'Diana', last: 'Prince' },
+      ],
+      actors: [
+        {
+          name: 'Christian Bale',
+          movies: ['The Dark Knight', 'Batman Begins'],
+        },
+        {
+          name: 'Di Caprio',
+          movies: ['Titanic', 'The Wolf of Wall Street'],
+        },
+      ],
+      myInfo: {
+        name: 'Vishwas',
+        channel: 'Codevolution',
+        course: 'Vue JS 3',
+      }
+    }
+  },
+}  
 </script>
 
 <style>
